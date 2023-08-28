613a144b3044263bdb339433c2abeb1caade434c
aksin <aksin@LAPTOP-NQLOFCJD>
2023-08-28 19:48:24 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Beabules.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
